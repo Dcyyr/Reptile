0b6c6ebc5d530b420e91cbb850aafeef265bab90
Linus Li <l17332426256@126.com>
2024-10-27 16:58:05 +0800

    修复了resize时会有黑色屏闪.

--- ReptileRoom/src/EditorLayer.cpp
+++ ReptileRoom/src/EditorLayer.cpp
@@ -36,6 +36,15 @@ namespace Reptile {
         // Update
         RP_PROFILE_FUNCTION();
 
+        //Resize
+        if (Reptile::FramebufferSpecification spec = m_Framebuffer->GetSpecification();
+            m_ViewportSize.x > 0.0f && m_ViewportSize.y > 0.0f && (spec.Width != m_ViewportSize.x || spec.Height != m_ViewportSize.y))
+        {
+            m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
+            m_CameraController.OnResize((float)m_ViewportSize.x, (float)m_ViewportSize.y);
+        }
+
+
         if(m_ViewportFocused)
             m_CameraController.OnUpdate(ts);
 
@@ -170,12 +179,8 @@ namespace Reptile {
             Application::Get().GetImGuiLayer()->BlockEvent(!m_ViewportFocused || !m_ViewportHovered);
 
             ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
-            if (m_ViewportSize != *(glm::vec2*)&viewportPanelSize)
-            {
-                m_Framebuffer->Resize((uint32_t)viewportPanelSize.x, (uint32_t)viewportPanelSize.y);
-                m_ViewportSize = { viewportPanelSize.x,viewportPanelSize.y };
-                m_CameraController.OnResize(viewportPanelSize.x, viewportPanelSize.y);
-            }
+           
+            m_ViewportSize = { viewportPanelSize.x,viewportPanelSize.y };
 
             uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
             ImGui::Image((void*)textureID, ImVec2{ viewportPanelSize.x, viewportPanelSize.y },ImVec2{0,1},ImVec2{1,0});
